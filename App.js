5a6020b497748e49008d82cf4392c05d8dcc09bd
Pham Van <thang_phamvan@nuance.com>
2018-05-10 09:09:09 +0700

    package name change

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {View, Text} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>ThangPhamVan</Text>
+      </View>
+    );
+  }
+}
