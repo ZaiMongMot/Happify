84957f9018bef6f49ab998229b08fbe2dcc3aa6f
Pham Van <thang_phamvan@nuance.com>
2018-05-12 23:56:06 +0700

    app running

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import {View, Text} from 'react-native';
-
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+  Text
+} from 'native-base';
+import  getTheme from "./native-base-theme/components";
+import material from "./native-base-theme/variables/material";​
 export default class App extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>ThangPhamVan</Text>
-      </View>
+
+    render() {
+    return ( 
+    <Container>
+      <Header>
+      <Left>
+      <Button transparent>
+      <Icon name = 'menu' />
+      </Button> 
+      </Left> 
+      <Body>
+      <Title> Header </Title> 
+      </Body> 
+      <Right />
+      </Header> 
+      <Content>
+      <Text> This is Content Section </Text> 
+      </Content> 
+      <Footer>
+      <FooterTab>
+      <Button full>
+      <Text> Footer </Text> 
+      </Button> 
+      </FooterTab> 
+      </Footer> 
+      </Container>
     );
   }
 }
